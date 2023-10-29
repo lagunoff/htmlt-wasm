33e315203e477981bd1d2cce2aa2cea9d09f00d4
Vladislav <vlad.lagunoff@gmail.com>
2023-10-29 18:34:55 +0400

    Refactor DOM events

--- src/HtmlT/DOM.hs
+++ src/HtmlT/DOM.hs
@@ -4,14 +4,9 @@ import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Kind
-import Data.ByteString
-import GHC.TypeLits
-import GHC.Int
-import GHC.Generics
-import Data.ByteString.Char8 qualified as Char8
 import Data.List qualified as List
-import Data.Maybe
-import Data.Proxy
+import GHC.Generics
+import GHC.Int
 
 import "this" HtmlT.Base
 import "this" HtmlT.Marshal
@@ -32,128 +27,167 @@ defaultEventListenerOptions = EventListenerOptions
   , stop_propagation = False
   }
 
-on :: forall eventName. IsEventName eventName => EventListener eventName -> Html ()
-on k = addEventListener @eventName Nothing k
+on :: forall eventName. IsEventName eventName => HaskellCallback eventName -> Html ()
+on k = addEventListener (addEventListenerArgs @eventName) k
 
-onOptions
-  :: forall eventName. IsEventName eventName
-  => EventListenerOptions
-  -> EventListener eventName
-  -> Html ()
-onOptions elo k = addEventListener @eventName (Just elo) k
+data AddEventListenerArgs hsCallback = AddEventListenerArgs
+  { event_name :: Utf8
+  , listener_options :: EventListenerOptions
+  , mk_hs_callback :: hsCallback -> JValue -> JSM ()
+  , mk_js_callback :: EventListenerOptions -> CallbackId -> Expr
+  } deriving (Generic)
 
-addEventListener
-  :: forall eventName. IsEventName eventName
-  => Maybe EventListenerOptions
-  -> EventListener eventName
+addEventListener :: forall hsCallback
+  . AddEventListenerArgs hsCallback
+  -> hsCallback
   -> Html ()
-addEventListener melo k = do
+addEventListener args k = do
   wasmEnv <- lift ask
   let
-    symbolStr = Char8.pack $ (symbolVal (Proxy @eventName))
-    (_, eventName) = parseEventName symbolStr
-    mkExpr callbackId = AddEventListener (Arg 0 0) (Utf8 eventName)
-      (mkEventListener @eventName melo callbackId)
-  callbackId <- lift $ newCallbackEvent (local (const wasmEnv) . mkCallback @eventName k)
+    mkExpr callbackId = AddEventListener (Arg 0 0) args.event_name
+      (args.mk_js_callback args.listener_options callbackId)
+  callbackId <- lift $ newCallbackEvent (local (const wasmEnv) . args.mk_hs_callback k)
   modify \s -> s {rev_queue = mkExpr callbackId : s.rev_queue}
-  where
-    parseEventName :: ByteString -> (Maybe ByteString, ByteString)
-    parseEventName eventSpec =
-      case Char8.breakEnd (=='/') eventSpec of
-        (namespace, eventName)
-          | Char8.null (Char8.dropEnd 1 namespace) -> (Nothing, eventName)
-          | otherwise -> (Just (Char8.dropEnd 1 namespace), eventName)
 
-class KnownSymbol eventName => IsEventName eventName where
-  type EventListener eventName :: Type
-  mkCallback :: EventListener eventName -> JValue -> JSM ()
-  mkEventListener :: Maybe EventListenerOptions -> CallbackId -> Expr
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/click_event
+pointerEventArgs :: Utf8 -> AddEventListenerArgs (JSM ())
+pointerEventArgs event_name = AddEventListenerArgs
+  { event_name
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k _ -> k
+  , mk_js_callback = \opts callbackId ->
+    Lam $ RevSeq $ ExecCallback callbackId Null : applyListenerOptions opts
+  }
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+submitEventArgs :: AddEventListenerArgs (JSM ())
+submitEventArgs = AddEventListenerArgs
+  { event_name = "submit"
+  , listener_options = defaultSubmitOptions
+  , mk_hs_callback = \k _ -> k
+  , mk_js_callback = \opts callbackId ->
+    Lam $ RevSeq $ ExecCallback callbackId Null : applyListenerOptions opts
+  }
+  where
+    defaultSubmitOptions = EventListenerOptions True True
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+inputEventArgs :: AddEventListenerArgs (Utf8 -> JSM ())
+inputEventArgs = AddEventListenerArgs
+  { event_name = "input"
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k j -> forM_ (fromJSVal j) k
+  , mk_js_callback = \opts callbackId -> Lam $ RevSeq
+    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
+    : applyListenerOptions opts
+  }
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event
+keyboardEventArgs :: Utf8 -> AddEventListenerArgs (Int64 -> JSM ())
+keyboardEventArgs event_name = AddEventListenerArgs
+  { event_name
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k j -> forM_ (fromJSVal j) k
+  , mk_js_callback = \opts callbackId -> Lam $ RevSeq
+    $ ExecCallback callbackId (Arg 0 0 `Dot` "keyCode")
+    : applyListenerOptions opts
+  }
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/focus_event
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/blur_event
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/focusin_event
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/focusout_event
+focusEventArgs :: Utf8 -> AddEventListenerArgs (JSM ())
+focusEventArgs event_name = AddEventListenerArgs
+  { event_name
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k _ -> k
+  , mk_js_callback = \opts callbackId ->
+    Lam $ RevSeq $ ExecCallback callbackId Null : applyListenerOptions opts
+  }
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+checkboxChangeEventArgs :: AddEventListenerArgs (Bool -> JSM ())
+checkboxChangeEventArgs = AddEventListenerArgs
+  { event_name = "change"
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k j -> forM_ (fromJSVal j) k
+  , mk_js_callback = \opts callbackId -> Lam $ RevSeq
+    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "checked")
+    : applyListenerOptions opts
+  }
+
+-- https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
+selectChangeEventArgs :: AddEventListenerArgs (Utf8 -> JSM ())
+selectChangeEventArgs = AddEventListenerArgs
+  { event_name = "change"
+  , listener_options = defaultEventListenerOptions
+  , mk_hs_callback = \k j -> forM_ (fromJSVal j) k
+  , mk_js_callback = \opts callbackId -> Lam $ RevSeq
+    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
+    : applyListenerOptions opts
+  }
+
+class IsEventName eventName where
+  type HaskellCallback eventName :: Type
+  addEventListenerArgs :: AddEventListenerArgs (HaskellCallback eventName)
 
 instance IsEventName "click" where
-  type EventListener "click" = JSM ()
-  mkCallback k _j = k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId Null
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "click" = JSM ()
+  addEventListenerArgs = pointerEventArgs "click"
+
+instance IsEventName "mousedown" where
+  type HaskellCallback "mousedown" = JSM ()
+  addEventListenerArgs = pointerEventArgs "mousedown"
+
+instance IsEventName "mouseup" where
+  type HaskellCallback "mouseup" = JSM ()
+  addEventListenerArgs = pointerEventArgs "mouseup"
 
 instance IsEventName "dblclick" where
-  type EventListener "dblclick" = JSM ()
-  mkCallback k _j = k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId Null
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "dblclick" = JSM ()
+  addEventListenerArgs = pointerEventArgs "dblclick"
 
 instance IsEventName "submit" where
-  type EventListener "submit" = JSM ()
-  mkCallback k _j = k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId Null
-    : applyListenerOptions (fromMaybe defaultSubmitOptions mopts)
-    where
-      defaultSubmitOptions = EventListenerOptions True True
+  type HaskellCallback "submit" = JSM ()
+  addEventListenerArgs = submitEventArgs
 
 instance IsEventName "input" where
-  type EventListener "input" = Utf8 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "input" = Utf8 -> JSM ()
+  addEventListenerArgs = inputEventArgs
 
 instance IsEventName "keydown" where
-  type EventListener "keydown" = Int64 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "keyCode")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "keydown" = Int64 -> JSM ()
+  addEventListenerArgs = keyboardEventArgs "keydown"
 
 instance IsEventName "keyup" where
-  type EventListener "keyup" = Int64 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "keyCode")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
-
-instance IsEventName "input/blur" where
-  type EventListener "input/blur" = Utf8 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
-
-instance IsEventName "input/focus" where
-  type EventListener "input/focus" = Utf8 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
-
-instance IsEventName "blur" where
-  type EventListener "blur" = JSM ()
-  mkCallback k _j = k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId Null
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "keyup" = Int64 -> JSM ()
+  addEventListenerArgs = keyboardEventArgs "keyup"
 
 instance IsEventName "focus" where
-  type EventListener "focus" = JSM ()
-  mkCallback k _j = k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId Null
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "focus" = JSM ()
+  addEventListenerArgs = pointerEventArgs "focus"
+
+instance IsEventName "blur" where
+  type HaskellCallback "blur" = JSM ()
+  addEventListenerArgs = pointerEventArgs "blur"
+
+instance IsEventName "input/blur" where
+  type HaskellCallback "input/blur" = Utf8 -> JSM ()
+  addEventListenerArgs = inputEventArgs {event_name = "blur"}
+
+instance IsEventName "input/focus" where
+  type HaskellCallback "input/focus" = Utf8 -> JSM ()
+  addEventListenerArgs = inputEventArgs {event_name = "focus"}
 
 instance IsEventName "checkbox/change" where
-  type EventListener "checkbox/change" = Bool -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "checked")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "checkbox/change" = Bool -> JSM ()
+  addEventListenerArgs = checkboxChangeEventArgs
 
 instance IsEventName "select/change" where
-  type EventListener "select/change" = Utf8 -> JSM ()
-  mkCallback k j = forM_ (fromJSVal j) k
-  mkEventListener mopts callbackId = Lam $ RevSeq
-    $ ExecCallback callbackId (Arg 0 0 `Dot` "target" `Dot` "value")
-    : applyListenerOptions (fromMaybe defaultEventListenerOptions mopts)
+  type HaskellCallback "select/change" = Utf8 -> JSM ()
+  addEventListenerArgs = selectChangeEventArgs
 
 applyListenerOptions :: EventListenerOptions -> [Expr]
 applyListenerOptions elo = List.concat
