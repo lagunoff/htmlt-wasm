655c56ae330b0c98b3aebbd42702fe600700fba8
Vladislav <vlad.lagunoff@gmail.com>
2023-11-04 17:04:28 +0400

    Provide a bit of documentation on JsMVar

--- src/HtmlT/MVar.hs
+++ src/HtmlT/MVar.hs
@@ -1,9 +1,13 @@
+{-| Defines the 'JsMVar' type along with operations to manipulate it.
+'JsMVar' is designed to be similar in spirit to the 'MVar' type from
+the base package. Useful for asynchronous interactions with browser
+APIs.
+-}
 module HtmlT.MVar where
 
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.IORef
-
 
 import "this" HtmlT.Protocol
 import "this" HtmlT.RJS
@@ -20,6 +24,10 @@ newEmptyJsMVar = do
   mvar_value <- liftIO $ newIORef Nothing
   return JsMVar {callback_id, mvar_value}
 
+-- | Blocking read operation on a `JsMVar`. If the `JsMVar` is empty,
+-- the thread is blocked until the `JsMVar` is filled with `writeJsMVar`.
+-- Only the last thread to call `takeJsMVar` on the same `JsMVar` will
+-- receive the value, all others will be garbage collected.
 takeJsMVar :: JsMVar a -> RJS a
 takeJsMVar mvar = go where
   go = do
@@ -28,8 +36,11 @@ takeJsMVar mvar = go where
       Nothing -> yield mvar.callback_id *> go
       Just val -> pure val
 
-putJsMVar :: JsMVar a -> a -> RJS ()
-putJsMVar mvar val = do
+-- | Performs a nonblocking write operation on a 'JsMVar'. This
+-- operation will wake up the last thread blocked by 'takeJsMVar', if
+-- any
+writeJsMVar :: JsMVar a -> a -> RJS ()
+writeJsMVar mvar val = do
   liftIO $ writeIORef mvar.mvar_value (Just val)
   evalExpr $ AsyncCallback mvar.callback_id Null
   return ()
